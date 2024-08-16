b932acb8f0cc04b2488fbace1b692f892f992d95
Sayed Saad <elsayed.saad2014@feps.edu.eg>
2024-08-17 02:24:22 +0300

    created floatingActionButton

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -7,9 +7,13 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        body: NotesViewBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        body: const NotesViewBody(),
       ),
     );
   }
