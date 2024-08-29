4db8b9d8bf4d34e607cd21111f1ffc78e710b7b3
Sayed Saad <elsayed.saad2014@feps.edu.eg>
2024-08-30 01:45:01 +0300

    Add field to bottom sheet

--- lib/views/widgets/add_notes_bottom_sheet.dart
+++ lib/views/widgets/add_notes_bottom_sheet.dart
@@ -14,7 +14,14 @@ class AddNoteBottomSheet extends StatelessWidget {
           SizedBox(
             height: 32,
           ),
-          CustomTextField(),
+          CustomTextField(
+            hint: 'Title',
+          ),
+          SizedBox(height: 16),
+          CustomTextField(
+            hint: 'Content',
+            maxLines: 5,
+          ),
         ],
       ),
     );
